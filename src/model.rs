5ada4cd584c7ab76000ffece876ff55d39f64f2d
莯凛 <mokurin000@gmail.com>
2026-06-24 07:38:24 +0800

    todo: fix label not displaying

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,9 @@
+use std::mem;
+
+use image::{DynamicImage, Rgba};
+use qrcode::EcLevel;
+use qrcode::render::Pixel;
+use spdlog::error;
 use winio::prelude::*;
 
 use crate::Result;
@@ -6,6 +12,7 @@ use crate::startup::Startup;
 pub struct MainModel {
     window: Child<Window>,
     canvas: Child<Canvas>,
+    label: Child<Label>,
 }
 
 pub enum MainMessage {
@@ -28,6 +35,10 @@ impl Component for MainModel {
                 size: Size::new(800.0, 600.0),
             },
             canvas: Canvas = (&window),
+            label: Label = (&window) => {
+                text: "Generate QR Code",
+                halign: HAlign::Center,
+            },
         }
 
         #[cfg(windows)]
@@ -35,7 +46,11 @@ impl Component for MainModel {
 
         window.show()?;
 
-        Ok(Self { window, canvas })
+        Ok(Self {
+            window,
+            canvas,
+            label,
+        })
     }
 
     async fn start(&mut self, sender: &ComponentSender<Self>) -> ! {
@@ -51,7 +66,7 @@ impl Component for MainModel {
 
     async fn update_children(&mut self) -> Result<bool> {
         // update the window
-        update_children!(self.window, self.canvas,)
+        update_children!(self.window, self.canvas, self.label)
     }
 
     async fn update(
@@ -74,23 +89,51 @@ impl Component for MainModel {
 
     fn render(&mut self, _sender: &ComponentSender<Self>) -> Result<()> {
         let csize = self.window.client_size()?;
-        self.canvas.set_size(csize)?;
 
-        let mut ctx = self.canvas.context()?;
+        {
+            let mut panel = layout! {
+                StackPanel::new(Orient::Vertical),
+                self.label,
+                self.canvas => { grow: true },
+            };
+            panel.set_size(csize)?;
+        }
+
         let is_dark = ColorTheme::current()? == ColorTheme::Dark;
-        let brush = SolidColorBrush::new(if is_dark {
-            Color::new(255, 255, 255, 255)
-        } else {
-            Color::new(0, 0, 0, 255)
-        });
-        let pen = BrushPen::new(&brush, 1.0);
 
-        ctx.draw_line(&pen, Point::zero(), Point::new(csize.width, csize.height))?;
-        ctx.draw_line(
-            &pen,
-            Point::new(0.0, csize.height),
-            Point::new(csize.width, 0.0),
-        )?;
+        match qrcode::QrCode::with_version("Hello, QRCode", qrcode::Version::Normal(40), EcLevel::H)
+        {
+            Err(e) => {
+                error!("Cannot generate QR: {e}");
+                self.label.set_text(format!("Error: {e}"))?;
+            }
+            Ok(qr) => {
+                let mut dark = Rgba::default_color(qrcode::Color::Dark);
+                let mut light = Rgba::default_color(qrcode::Color::Light);
+                if is_dark {
+                    mem::swap(&mut dark, &mut light);
+                }
+
+                let actual_size = self.canvas.size()?;
+                let max_dim = actual_size.height.min(actual_size.width) as u32;
+                let qr_image = qr
+                    .render::<image::Rgba<u8>>()
+                    .dark_color(dark)
+                    .light_color(light)
+                    .max_dimensions(max_dim, max_dim)
+                    .build();
+                let mut ctx = self.canvas.context()?;
+                let qr_size = Size::new(qr_image.width() as _, qr_image.height() as _);
+                let img = DynamicImage::ImageRgba8(qr_image);
+                let image = ctx.create_image(img)?;
+
+                let left_top = (actual_size - qr_size) / 2.0;
+                let rect = Rect::new(Point::new(left_top.width, left_top.height), qr_size);
+
+                ctx.draw_image(&image, rect, Some(Rect::new(Point::origin(), qr_size)))?;
+                ctx.close()?;
+            }
+        }
 
         Ok(())
     }
